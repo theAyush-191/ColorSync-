4ee2d518ac3edf6f8ae41c083946d16daa8dc1f1
theAyush-191 <ayush191.znr@gmail.com>
2025-07-30 16:29:26 +0530

    Initial Commit

---
+++ ColorSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSync
+//
+//  Created by Ayush Singh on 30/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
